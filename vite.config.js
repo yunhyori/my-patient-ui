cc2b86ece70f8fb3b81faafffcd377a97c3b58b6
yunhyori <y.sunmi8117@gmail.com>
2025-06-03 11:41:45 +0900

    최종 빌드 결과 및 UI 개선 반영

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ base: '/my-patient-ui/', plugins: [react()] })
+export default defineConfig({
+  base: '/my-patient-ui/',  
+  plugins: [react()],
+})
